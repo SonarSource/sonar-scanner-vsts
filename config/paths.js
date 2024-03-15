0d8d2516653e39c499fa1e2d22290b810d3f1c01
7PH <benjamin.raymond@sonarsource.com>
2024-03-15 15:45:13 +0100

    VSTS-335 Drop PowerShell SQ tasks (V3)

--- config/paths.js
+++ config/paths.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const fs = require('fs-extra');
+const path = require("path");
+const fs = require("fs-extra");
 
 // Make sure any symlinks in the project folder are resolved:
 // https://github.com/facebookincubator/create-react-app/issues/637
@@ -10,43 +10,37 @@ function resolveRelativePath(relativePath) {
 }
 exports.resolveRelativePath = resolveRelativePath;
 
-exports.pathAllFiles = function(...paths) {
-  return path.join(...paths, '**', '*');
+exports.pathAllFiles = function (...paths) {
+  return path.join(...paths, "**", "*");
 };
 
-const buildPath = resolveRelativePath('build');
-const commonPath = resolveRelativePath('common');
-const commonv5Path = resolveRelativePath('commonv5');
-const extensionsPath = resolveRelativePath('extensions');
+const buildPath = resolveRelativePath("build");
+const commonPath = resolveRelativePath("common");
+const commonv5Path = resolveRelativePath("commonv5");
+const extensionsPath = resolveRelativePath("extensions");
 
 exports.paths = {
   root: appDirectory,
   build: {
     root: buildPath,
     extensions: {
-      root: path.join(buildPath, 'extensions'),
-      tasks: path.join(buildPath, 'extensions', '**', 'tasks'),
-      sonarqubeTasks: path.join(buildPath, 'extensions', 'sonarqube', 'tasks'),
-      sonarcloudTasks: path.join(buildPath, 'extensions', 'sonarcloud', 'tasks')
+      root: path.join(buildPath, "extensions"),
+      tasks: path.join(buildPath, "extensions", "**", "tasks"),
+      sonarqubeTasks: path.join(buildPath, "extensions", "sonarqube", "tasks"),
+      sonarcloudTasks: path.join(buildPath, "extensions", "sonarcloud", "tasks"),
     },
-    classicScanner: path.join(buildPath, 'tmp', 'classic-sonar-scanner-msbuild'),
-    dotnetScanner: path.join(buildPath, 'tmp', 'dotnet-sonar-scanner-msbuild')
+    classicScanner: path.join(buildPath, "tmp", "classic-sonar-scanner-msbuild"),
+    dotnetScanner: path.join(buildPath, "tmp", "dotnet-sonar-scanner-msbuild"),
   },
-  common: {
-    old: path.join(commonPath, 'powershell'),
-    new: path.join(commonPath, 'ts')
-  },
-  commonv5: {
-    new: path.join(commonv5Path, 'ts')
-  },
+  common: path.join(commonPath, "ts"),
+  commonv5: path.join(commonv5Path, "ts"),
   extensions: {
     root: extensionsPath,
     tasks: {
-      root: path.join(extensionsPath, '**', 'tasks'),
-      old: path.join(extensionsPath, '**', 'tasks', '**', 'old'),
-      scv1: path.join(extensionsPath, '**', 'tasks', '**', 'v1'),
-      v4: path.join(extensionsPath, '**', 'tasks', '**', 'v4'),
-      v5: path.join(extensionsPath, '**', 'tasks', '**', 'v5')
-    }
-  }
+      root: path.join(extensionsPath, "**", "tasks"),
+      scv1: path.join(extensionsPath, "**", "tasks", "**", "v1"),
+      v4: path.join(extensionsPath, "**", "tasks", "**", "v4"),
+      v5: path.join(extensionsPath, "**", "tasks", "**", "v5"),
+    },
+  },
 };
